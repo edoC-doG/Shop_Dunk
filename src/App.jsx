f4906130eda34bc9ab079928b4527a483cab6394
MinhLong <longbo1208@gmail.com>
2024-08-05 16:27:44 +0700

    iPhone 3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HighLight from "./components/HighLight";
+import Model from "./components/Model";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <HighLight />
+      <Model />
     </main>
   );
 }
